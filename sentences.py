636e984aeccc586d2b60e4f07513c1c23fe25ef3
gellel <lindsay.gelle@gmail.com>
2016-11-17 18:33:19 +1100

    Created sentences class.

---
+++ sentences.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#sys.argv[1:]
+
+###################################
+### python scripts dependencies ###
+###################################
+### import pretty strings class
+from strings import String
+
+
+
+
+class Sentence (String):
+
+	def __init__ (self, **kwargs)
+		pass
+
+
+
+if __name__ == '__main__':
+
+	print Sentence
