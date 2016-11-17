fb9f2ab9b5a97783a0ecba5874208bea0ae05ffc
gellel <lindsay.gelle@gmail.com>
2016-11-17 17:48:31 +1100

    Editing set to be defined a selections.

---
+++ selections.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#sys.argv[1:]
+
+###################################
+### python scripts dependencies ###
+###################################
+### import messages class
+from messages import Message
+
+
+
+class Select (Message):
+
+
+	def __init__ (self, **kwargs):
+		pass
+
+
+
+
+if __name__ == '__main__':
+
+	print Select
