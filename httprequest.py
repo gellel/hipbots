64916e2fd85fb2e438754ead6bd9b178cee120b8
gellel <lindsay.gelle@gmail.com>
2016-11-21 09:04:20 +1100

    Splitting http requests into seperate file.

---
+++ httprequest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#sys.argv[1:]
+
+###################################
+### python scripts dependencies ###
+###################################
+### py requests class package
+import requests
+
+
+
+
+if __name__ == '__main__':
+
+	print requests
