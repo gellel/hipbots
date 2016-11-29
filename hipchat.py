7cabb2c446b785f7d36b21a7d7d33f6c6d93d23f
gellel <lindsay.gelle@gmail.com>
2016-11-29 17:37:47 +1100

    Editing public functions.

--- hipchat.py
+++ hipchat.py
@@ -8,23 +8,36 @@
 
 class HTML:
 
+	########################################################
+	### public class for testing support HTML in HipChat ###
+	########################################################
+
 	ELEMENTS = ['A', 'B', 'I', 'STRONG', 'EM', 'BR', 'IMG', 'PRE', 'CODE', 'OL', 'UL', 'LI', 'TABLE', 'TR', 'TD', 'TH', 'TF', 'SPAN']
 	ATTRIBUTES = {'A':['HREF','REL','DATA-TARGET','DATA-TARGET-OPTIONS'], 'IMG':['SRC','ALT','WIDTH','HEIGHT','ALIGN','STYLE'],'TD':['COLSPAN','ROWSPAN','VALIGN'],'TR':['VALIGN'],'TH':['COLSPAN','ROWSPAN','VALIGN'],'SPAN':['STYLE']}
 
+	@staticmethod
+	def element (element = 'A'):
+		### @description: public function confirms HTML tag supported in HipChat
+		### @parameter: <element>, @type: <str>, @default: <str>
+		### @return: @type: <str>
+
+		# confirm tag is supported 
+		return True if str(element).upper() in HTML.ELEMENTS else False
 
 	@staticmethod
-	def confirm ():
-		pass
-
-	@staticmethod
-	def element (arg):
-		### @description: confirms HTML tag supported in HipChat
-		pass
-
-	@staticmethod
-	def attributes (arg):
-		### @description: confirms HTML attribute supported in HipChat
-		pass
+	def attributes (**kwargs):
+		### @description: public function confirms HTML attribute supported in HipChat
+		### @parameter: <kwargs>, @type: <dict>
+		### @return: @type: <bool>
+		
+		# set base element
+		# @parameter: <element>, @type: <str>, @default: <str>
+		element = kwargs.get('element', 'A')
+		# set base attribute
+		# @parameter: <node>, @type: <str>, @default: <str>
+		attribute = kwargs.get('attribute', 'HREF')
+		# confirm attribute is supported
+		return True if str(element).upper() in HTML.ELEMENTS and attribute in HTML.ATTRIBUTES[str(element).upper()] else False
 
 
 
@@ -51,4 +64,4 @@ class REST:
 
 if __name__ == '__main__':
 	
-	pass
+	print HTML.attributes(element = 'SPAN')
